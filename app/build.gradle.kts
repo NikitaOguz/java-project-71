3f88b17df199642d35eab0d9b3900ac0f122e7cd
Никита Кобытев <nikitakobytev@mail.ru>
2026-02-25 23:56:26 +0700

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher") // ← ВОТ ЭТО ДОБАВЬ
 }
 
 tasks.test {
